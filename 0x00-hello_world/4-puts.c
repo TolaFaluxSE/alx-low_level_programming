1222fef37b29a8e4d7ae44d1ac196b105f8e406f
TolaFaluxSE <tolafalux@gmail.com>
2022-09-08 17:00:28 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
-/**
-* main - Entry point
-*
-* Return: Always 0 (Success)
-*/
+/*main - entry point */
+/*Return: Always 0 */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
+  puts("\"Programming is like building a multilingual puzzle\n");
   return (0);
 }
