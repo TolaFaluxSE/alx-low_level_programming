9a4b9485f115505ceb6e7ca8682f25a7a64f7100
TolaFaluxSE <tolafalux@gmail.com>
2022-09-11 07:30:23 -0700

    print two two-digit number

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -3,9 +3,9 @@
 #include <time.h>
 
 /**
-* main - print double digit combos.
+* main - print triple digit combos.
 *
-* Description: print double digit separated by
+* Description: print triple digit separated by
 * comma and space.
 *
 * Return: Always 0 (Success)
@@ -13,28 +13,42 @@
 int main(void)
 
 {
-int i, j;
+int i, j, k, m;
 i = 48;
-j = 48;
 
 while (i < 58)
 {
-j = i + 1;
+j = 48;
 while (j < 58)
+{
+m = j + 1;
+k = 1;
+while (k < 58)
+{
+
+while (m < 58)
 {
 putchar(i);
 putchar(j);
+putchar(32);
+putchar(k);
+putchar(m);
 
-if (i < 56 || j < 57)
+if (i < 57 || j < 56 || k < 57 || m < 57)
 {
 putchar(44);
 putchar(32);
 }
-j++;
+m++;
 
 }
-
+m = 48;
+k++;
+}
+j++;
+}
 i++;
+
 }
 putchar(10);
 
