9f294b90d9d1c93128c740960777221773a6ae3e
TolaFaluxSE <tolafalux@gmail.com>
2022-09-11 02:28:55 -0700

    program that prints the alphabet in lowercase and uppercase

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,18 +5,32 @@
 * Main : alphabets to be in uppercase & lowercase      
 *
 * Description: Using the putchar function to print alphabet
-* in lowercase & uppercase                                  
+* in lowercase & uppercase
+* ASCII value, uppercase alphabets - 65 to 90
+* ASCII value, lowercase - 97 to 122
 * it is executed.
 *
 * Return: Always 0 (Success)
 */
 int main(void)  
 {
-char chr ='a';
-for (chr = 'a'; chr <= 'z'; chr++)
-putchar(chr);
-char ch ='A';
-for (ch = 'A'; ch <= 'Z'; ch++)
-putchar(ch);
-return (0);  
+int a, A;
+a = 97;
+A = 65;
+
+while (a < 123)
+{
+putchar(a);
+a++;
+}
+
+while (A < 91)
+{
+putchar(A);
+A++;
+}
+
+putchar(10);
+
+return (0);
 }
