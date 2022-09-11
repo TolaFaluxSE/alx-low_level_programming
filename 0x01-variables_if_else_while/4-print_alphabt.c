2a31f886f1df256d435a2ae1dd1c18e9c2a90eb7
TolaFaluxSE <tolafalux@gmail.com>
2022-09-11 03:32:22 -0700

    program that prints the alphabet in lowercase except letters e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - send alphabets to be in lowercase
+*
+* Description: Using the putchar function to print alphabet* in lowercase
+* it is executed.
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+
+{
+int i = 97;
+while (i < 123)
+{
+{if (i != 101 && i != 113)
+{putchar(i);
+}
+i++;
+}
+putchar(10);
+return (0);
+}
