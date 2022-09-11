8532f785856c211cd0fa7ce54b18ef8d48893600
TolaFaluxSE <tolafalux@gmail.com>
2022-09-11 13:19:16 -0700

    print two two-digit numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,24 +1,12 @@
-/*
-
-* File: 102-print_comb5.c
-
-* Auth: Brennan D Baraban
-*/
-
-
-
 #include <stdio.h>
 
 /**
-
 * main - Prints all possible combinations of two two-digit numbers,
-
 *        ranging from 0-99, separated by a comma followed by a space.
-
 *
-
+* Description: print two two digit numbers
+*
 * Return: Always 0.
-
 */
 
 int main(void)
