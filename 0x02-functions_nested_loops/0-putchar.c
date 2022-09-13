b8b8c439aa441450e3a9882d3cf354a69e30b043
TolaFaluxSE <tolafalux@gmail.com>
2022-09-13 14:37:15 -0700

    prints _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,15 @@
 #include "main.h"
 
+
 /**
 * main - print _putchar followed by a new line
 *
 * Description - print _putchar in file 0-putchar.c
 *
 */
+
+int _putchar(char c)
+{
+  return 0;
+
+}
