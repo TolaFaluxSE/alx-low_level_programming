aa5e214c0739bcaad5f293e347dbbd5c84219dfd
TolaFaluxSE <tolafalux@gmail.com>
2022-09-13 14:46:48 -0700

    prints _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,1 @@
 #include "main.h"
-#include <unistd.h>
-
-/**
-* main - print _putchar followed by a new line
-*
-* Description - print _putchar in file 0-putchar.c
-*
-*/
-
-int _putchar(char c)
-{
-  return (write(1, &c, 1));
-
-}
