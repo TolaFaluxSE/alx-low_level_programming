54d31a0b7811e6faaaff59bf2a423097b457764c
TolaFaluxSE <tolafalux@gmail.com>
2022-09-14 13:47:56 -0700

    prints _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -12,11 +12,11 @@
 int main(void)
 
 {
-char text[10] = "_putchar";
+char text[10] = "_putchar ";
 int i = 0;
 for (i = 0; i < 9; i++)
 
-{_putchar("text[i]\n");
+{_putchar(text[i]);
 }
 return (0);
 }
