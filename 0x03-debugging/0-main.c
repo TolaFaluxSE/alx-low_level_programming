4e4078d4202064d647c0e3b4f8b7c70767168350
TolaFaluxSE <tolafalux@gmail.com>
2022-09-15 12:47:04 -0700

    debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* main - tests function that prints if integer is +ve or -ve
+*
+* Description : follow steps to print integer as +ve or -ve
+*
+* Return: 0
+*/
+
+int main (void)
+
+{
+int i;
+i - 98;
+positive_or_negative(i);
+
+return (0);
+
+}
