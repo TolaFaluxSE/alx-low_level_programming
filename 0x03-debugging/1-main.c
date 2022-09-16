17bf57ff52c712bb92fc1572f00caba31c5a02e0
TolaFaluxSE <tolafalux@gmail.com>
2022-09-16 02:16:17 -0700

    causes an infinite loop

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -9,7 +9,6 @@ int main(void)
 {
 
 int i;
-
 printf("Infinite loop incoming :(\n");
 
 i = 0;
@@ -19,7 +18,6 @@ i = 0;
 * putchar(i);
 * }
 */
- 
 printf("Infinite loop avoided! \\o/\n");
 
 return (0);
