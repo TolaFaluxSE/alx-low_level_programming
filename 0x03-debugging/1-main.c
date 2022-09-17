aaa48b4087291c0b442be4abd51399711fe53a46
TolaFaluxSE <tolafalux@gmail.com>
2022-09-16 18:03:11 -0700

    causes an infinite loop

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -6,19 +6,19 @@
 */
 
 int main(void)
-
 {
 int i;
-printf("Infinite loop incoming :(\n");
-i = 0;
 
-/**
-* while (i < 10)
-* { Description - causes & avoid infinite loop
-* putchar(i);
-* }
+printf("Infinite loop incoming :(\n");
+
+i = 0;
+/*
+*while (i < 10)
+*{
+*putchar(i);
+*}
 */
-printf("Infinite loop avoided! \\o/\n"); /* \\o/ to avoid infinite loop */
+printf("Infinite loop avoided! \\o/\n");
 
 return (0);
 }
