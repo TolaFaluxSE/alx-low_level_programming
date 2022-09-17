b7f93cd7e2f1aecb3a5d3c24a4d8582e6fec5b0b
TolaFaluxSE <tolafalux@gmail.com>
2022-09-16 17:31:51 -0700

    function that prints an integer.

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,66 +1,23 @@
-#include <math.h>
+#include <stdio.h>
 #include "main.h"
 
 /**
-* power - exponents
-* @base: base
-* @exp: exponent
-* Return: result (int)
-*/
-
-int  power(int base, int exp)
-{
-int i, num;
-num = 1;
-for (i = 0; i < exp; ++i)
-num *= base;
-return (num);
-}
-
-/**
-* print_number - prints an integer
-* @n: number to print
-* Return void
+* print_number - Prints an integer.
+* @n: The integer to printed
 */
 
 void print_number(int n)
 {
-int negative = 0;
-int digit;
-int divisor;
-int begin = 0;
-int place = 10;
+unsigned int num = n;
+/* first check if its negative */
 if (n < 0)
 {
-negative = 1;
-n = n * -1;
-}
-while (place >= 0)
-{
-/*divisor = pow(10, place);*/
-divisor = power(10, place);
-digit = ((n / divisor) % 10);
-if (digit == 0 && begin == 0)
-{
-place--;
-}
-else if (digit != 0 && begin == 0)
-{
-begin = 1;
-if (negative == 1)
 _putchar('-');
-_putchar('0' + digit);
-place--;
+num = -num;
 }
-else
-{
-_putchar('0' + digit);
-place--;
-}
-}
-if (digit == 0 && divisor == 1)
-{
-_putchar(48);
-}
-
+/* print the first few digits */
+if ((num / 10) > 0)
+print_number(num / 10);
+/* print the last digit */
+_putchar((num % 10) + 48);
 }
