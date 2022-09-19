8351c1035a63b734ce7d4d926a79eaf448f2c5f4
TolaFaluxSE <tolafalux@gmail.com>
2022-09-19 15:41:10 -0700

    prints every character of a string, starting with first character

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,19 +1,20 @@
 #include "main.h"
 
 /**
- *
- *
- *
- */
+* puts2 - print alternating chars of string
+* @str: string
+* retun: void
+*/
 
 void puts2(char *str)
 {
 int i;
+
 i = 0;
 while (str[i] != '\0')
 {
 if (i % 2 == 0)
-_putchar(str[1]);
+_putchar(str[i]);
 i++;
 }
 _putchar('\n');
