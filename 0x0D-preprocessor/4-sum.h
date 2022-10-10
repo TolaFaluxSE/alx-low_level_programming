1a0a1ef25fd700f95e83f2d3b44616a056fb45e8
TolaFaluxSE <tolafalux@gmail.com>
2022-10-10 14:06:28 -0700

    function-like macro SUM(x, y) that computes the sum of the numbers x and y

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,19 @@
+#ifndef SUM_H
+#define SUM_H
+
+
+
+/**
+* File: 4-sum.h
+* Auth: TolaFaluxSE
+* Desc: Header file that defines a function-like macro SUM(x, y)
+*       that computes the sum of the numbers x and y.
+*/
+
+
+
+#define SUM(x, y) ((x) + (y))
+
+
+
+#endif
