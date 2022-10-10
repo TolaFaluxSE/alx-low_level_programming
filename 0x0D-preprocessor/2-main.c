1d423f28d7c19332c33f23ea98942185c4efb098
TolaFaluxSE <tolafalux@gmail.com>
2022-10-10 13:45:07 -0700

    :wq

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+
+/**
+* main - Prints the name of the file of the program
+* was compiled from, followed by a new line.
+*
+* file_name: 2-main.c
+* Return: Always 0.
+*/
+
+int main(void)
+{
+printf("%s\n", __FILE__);
+return (0);
+}
