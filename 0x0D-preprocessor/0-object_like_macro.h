08ccd9d686e948bd1c3c8255961e60933eb71c9b
TolaFaluxSE <tolafalux@gmail.com>
2022-10-10 15:18:08 -0700

    preprocessor

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,11 +1,5 @@
-#ifndef OBJECT_LIKE_MACRO_H
-#define OBJECT_LIKE_MACRO_H
-
-/**
-* File: 0-object_like_macro.h
-* Desc: Header file that defines a macro named
-*       SIZE as an abbreviation for the token 1024.
-*/
+#ifndef _0_OBJECT_LIKE_MACRO_H_
+#define _0_OBJECT_LIKE_MACRO_H_
 
 
 
