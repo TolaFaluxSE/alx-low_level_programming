548947e1df527f4349c6604e1c9151b52e8a8434
TolaFaluxSE <tolafalux@gmail.com>
2022-10-30 07:46:28 -0700

    Project on C function_pointers

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -60,6 +60,7 @@ int op_div(int a, int b)
 if (b == 0)
 {
 printf("Error\n");
+
 exit(100);
 }
 return (a / b);
@@ -79,6 +80,7 @@ int op_mod(int a, int b)
 if (b == 0)
 {
 printf("Error\n");
+
 exit(100);
 }
 return (a % b);
